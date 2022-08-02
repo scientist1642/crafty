710b0e93087cbdbde32bf344f5997cadc7a4da7d
Zura Isakadze <z.isakadze@gmail.com>
2022-08-02 13:06:06 +0400

    New import location for asyncstorage

--- src/contexts/FavoriteContext.js
+++ src/contexts/FavoriteContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const FavoriteContext = createContext();
 
